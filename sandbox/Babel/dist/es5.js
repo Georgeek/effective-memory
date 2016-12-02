38bc950991516f77be2e421555784f4f11e1411d
Egor Madiarov <egormad@gmail.com>
2016-12-02 19:00:29 +0800

    block with carousels
    no JS, only CSS

---
+++ sandbox/Babel/dist/es5.js
@@ -0,0 +1,2 @@
+"use strict";function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function")}}var Person=function Person(){_classCallCheck(this,Person)};
+'use strict';var print=function print(message){return console.log(message)};print('\u041F\u0440\u0438\u0432\u0435\u0442 Babel!!!!');
